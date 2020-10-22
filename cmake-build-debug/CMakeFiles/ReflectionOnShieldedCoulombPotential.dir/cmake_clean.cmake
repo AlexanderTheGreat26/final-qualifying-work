f9715e4e8281cbe73aad21c68704f371e5ad3bcd
AlexanderTheGreat26 <kaegod@yandex.ru>
2020-10-22 10:07:39 +0300

    v0.0.1

---
+++ cmake-build-debug/CMakeFiles/ReflectionOnShieldedCoulombPotential.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReflectionOnShieldedCoulombPotential.dir/main.cpp.o"
+  "ReflectionOnShieldedCoulombPotential"
+  "ReflectionOnShieldedCoulombPotential.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReflectionOnShieldedCoulombPotential.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
